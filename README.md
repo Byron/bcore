446d4de234d5f8e282f066c7194ad0d59dc7f780
Sebastian Thiel <byronimo@gmail.com>
2014-03-11 17:34:32 +0100

    Updated readme to state what should be contained in the core framework, and removed obsolete module

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 This repository is a core library of commonly used python facilities.
 
-### TODO
+Those include 
 
-Much more introductory information
+- bootstrapper for dependency handling and pre-runtime configuration of programs
+- file based, cascading key-value store with strong schema 
+- service registry for plugins and configuration
+- Path type for object oriented path handling
+- small utility types too numerous to mention
+
+As such, core provides everything a basic program needs to run, as it helps to take care about handling of program dependencies, program configuration 
--- python/bcore/qc/base.py
+++ python/bcore/qc/base.py
@@ -14,7 +14,6 @@ import traceback
 
 from bcore import abstractmethod
 from bcore import InterfaceBase
-from bcore.decorators import return_member
 from .interfaces import (
                          IQualityCheck,
                          IQualityCheckRunner,
@@ -24,6 +23,43 @@ from .interfaces import (
 
 
 import bcore.core.logging
+
+# ==============================================================================
+## @name Decorators
+# ------------------------------------------------------------------------------
+## @{
+
+## @note looks like this could be done with properties as well
+## @note also, from the name of this decorator it's not apparent that the value can't be None
+##       that would be better handled by not hasattr -> raise error, value is None -> return value
+##       and a seperate not_none decorator
+
+def return_member(name):
+    """A decorator returning a member with `name` of the input instance
+    @note raise if the member does not exist or is None"""
+    def function_receiver(fun):
+        """receives the actual function to wrap"""
+        @wraps(fun)
+        def wrapper(instance):
+            """implements obtaining the member"""
+            try:
+                rval = getattr(instance, name)
+                if rval is None:
+                    raise AttributeError
+                return rval
+            except AttributeError:
+                cls = instance
+                if not isinstance(instance, type):
+                    cls = type(instance)
+                #end obtain class instance
+                raise AssertionError("Class %s has to set the %s member" % (cls.__name__, name))
+            #end exception handling
+        #end wrapper
+        return wrapper
+    #end function receiver
+    return function_receiver
+
+## -- End Decorators -- @}
 
 
 class NoCategory(QualityCheckCategory):
