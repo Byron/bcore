aa4db0fc45327b16134870af259e8b8d27b51126
Sebastian Thiel <byronimo@gmail.com>
2014-03-17 20:12:07 +0100

    Removed all GUI related code into its own project, bqt

--- python/bcore/gui/widgets/__init__.py
+++
@@ -1,9 +0,0 @@
-#-*-coding:utf-8-*-
-"""
-@package bcore.gui.widgets
-@brief A place where each module is a general purpose widget for use in composite GUIs
-
-@copyright 2013 Sebastian Thiel
-"""
-from .plugin_selector import *
-from .messagebox import *
--- python/bcore/gui/widgets/ui/__init__.py
+++
@@ -1,9 +0,0 @@
-#-*-coding:utf-8-*-
-"""
-@package bcore.gui.widgets.ui
-@brief A place for precompiled .ui files including their designer source file
-
-@copyright 2013 Sebastian Thiel
-"""
-
-from .selector_ui import *
