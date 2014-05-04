44580021b7b279a6f16f3d9c2bf2669e6bc3d704
Sebastian Thiel <byronimo@gmail.com>
2014-05-04 18:01:16 +0200

    Added setup.py to allow pip to install from the source repository

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+pkg_root = 'src/python'
+
+setup(name='bcore',
+      version='0.1.0',
+      description='The bcore project is designed to help writing powerful and maintainable application rapidly.',
+      author='Sebastian Thiel',
+      author_email='byronimo@gmail.com',
+      url='https://github.com/Byron/bcore',
+      packages=find_packages(pkg_root),
+      package_dir={'' : pkg_root},
+     )
