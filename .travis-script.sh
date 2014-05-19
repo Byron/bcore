4d3a22442f35301a904dbd8872e031928f25cc80
Sebastian Thiel <byronimo@gmail.com>
2014-05-19 18:59:08 +0200

    which environment variables are actually set ?? Docs don't seem to match

--- .travis-script.sh
+++ .travis-script.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # NOTE: this script must be executed from bdevel travis tests, assuming to be at the bdevel root
 
-context=lib/components/core/src/python/bcmd/tests
-bin/posix/be @$context go daemon-command || exit
+context=src/python/bcmd/tests
+be @$context go daemon-command || exit
 # daemonize it
 [ ! -f pidfile ] || exit
-bin/posix/be @$context go daemon-command -d pidfile || exit
+be @$context go daemon-command -d pidfile || exit
 
 # when it returned, pid file sholdn't exist anymore - it's hard to test for it in this async mode we are in 
 [ ! -f pidfile ] || exit
